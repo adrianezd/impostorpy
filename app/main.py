a5170928d682f7f3531e0135778c390378613790
adrianezd <adrianezquerrods@gmail.com>
2025-08-28 23:30:24 +0200

    main working and structure

--- app/main.py
+++ app/main.py
@@ -14,18 +14,28 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
     async def broadcast(self, message: str):
         for connection in self.active_connections:
             await connection.send_text(message)
 
-manager = ConnectionManager()
 
-@app.websocket("/ws/{room_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id: str):
+manager = ConnectionManager()
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(f"{room_id}: {data}")
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
         manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} disconnected")
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
